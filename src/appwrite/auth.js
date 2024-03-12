f43a6a58210370a1421edfb45b1fb64d241fae1b
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-12 19:25:26 +0600

    Implements login authentication system successfully

--- src/appwrite/auth.js
+++ src/appwrite/auth.js
@@ -1,4 +1,6 @@
-import { Account, Client } from "appwrite";
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-useless-catch */
+import { Account, Client, ID } from "appwrite";
 import conf from "../conf/conf";
 
 export class AuthService {
@@ -10,5 +12,24 @@ export class AuthService {
       .setEndpoint(conf.appwriteUrl)
       .setEndpoint(conf.appwriteProjectId);
     this.account = new Account(this.client);
+  }
+
+  async createAccount({ email, password, name }) {
+    try {
+      const userAccount = await this.client.create(ID, email, password, name);
+      if (userAccount) {
+        //implements another method
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async login({ email, password }) {
+    try {
+      return await this.client.login(email, password);
+    } catch (error) {
+      throw error;
+    }
   }
 }
