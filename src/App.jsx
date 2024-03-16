36808338762c52d942a76d87024e4ac64f667910
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-16 14:14:03 +0600

    Implements dispatch at app file

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,40 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react";
 import "./App.css";
+import { useDispatch } from "react-redux";
+import authService from "../src/appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import Header from "./components/Header/Header";
+import Footer from "./components/Header/Footer/Footer";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return (
-    <>
-      <h1>Blog App</h1>
-    </>
-  );
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, [dispatch]);
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        <main>
+          TODO: <Outlet />
+        </main>
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
 export default App;
