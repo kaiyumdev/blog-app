95ef4c32b78e7ed150de064046ea057c17bbcab6
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-17 11:12:44 +0600

    Implements redux store for state management

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { authSlice } from "./authSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    authSlice,
+  },
 });
