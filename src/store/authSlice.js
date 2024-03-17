c4af3a9a64fcf8a45087ab32fcfd835f8a58553e
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-17 11:19:41 +0600

    Implements authSLice for blog-app project

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -1,6 +1,6 @@
-/* eslint-disable no-unused-vars */
 import { createSlice } from "@reduxjs/toolkit";
 
+/* eslint-disable no-unused-vars */
 const initialState = {
   status: false,
   userData: null,
@@ -14,7 +14,7 @@ export const authSlice = createSlice({
       state.status = true;
       state.userData = action.payload.userData;
     },
-    logout: (state) => {
+    logout: (state, action) => {
       state.status = false;
       state.userData = null;
     },
