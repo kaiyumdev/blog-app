369e3b50016db7c127e8b4751b144a405558fd6a
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-18 15:14:16 +0600

    Implements index for better export experience

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+/* eslint-disable no-unused-vars */
+import Header from "./Header/Header";
+import Footer from "./Footer/Footer";
+import Container from "./container/Container";
+import Logo from "./Logo";
+
+export { Header, Footer, Container, Logo };
