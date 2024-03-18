91298a4b090ab35030001ebbe9d71108e6dd90bf
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-18 06:51:39 +0600

    Create logo for this project

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React from "react";
+
+function Logo({ width = "100px" }) {
+  return <div>Logo</div>;
+}
+
+export default Logo;
