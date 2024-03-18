6c43b1635a3e73afab3c9ca137327e8fb351d672
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-19 00:53:25 +0600

    Create header section by implementing navItems dynamically

--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,6 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
 
-export { Header, Footer, Container, Logo };
+export { Header, Footer, Container, Logo, LogoutBtn };
