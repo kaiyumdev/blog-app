32a58d0b7899866802596471d27debc97aa2472e
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-25 02:55:52 +0600

    feat(signup): Implements logo and link login button at signup file

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -2,9 +2,10 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import authService from "../appwrite/auth";
 import { login } from "../store/authSlice";
+import Logo from "./Logo";
 
 const Signup = () => {
   const navigate = useNavigate();
@@ -27,7 +28,32 @@ const Signup = () => {
       setError(error.message);
     }
   };
-  return <div>Signup</div>;
+  return (
+    <div className="flex items-center justify-center">
+      <div
+        className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
+      >
+        <div className="mb-2 flex justify-center">
+          <span className="inline-block w-full max-w-[100px]">
+            <Logo width="100%" />
+          </span>
+        </div>
+        <h2 className="text-center text-2xl font-bold leading-tight">
+          Sign up to create account
+        </h2>
+        <p className="mt-2 text-center text-base text-black/60">
+          Already have an account?&nbsp;
+          <Link
+            to="/login"
+            className="font-medium text-primary transition-all duration-200 hover:underline"
+          >
+            Sign In
+          </Link>
+        </p>
+        {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+      </div>
+    </div>
+  );
 };
 
 export default Signup;
