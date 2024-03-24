cb37f4e4e744ad5e4a761f1faeddcbe2fae73d5c
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-25 02:23:38 +0600

    feat(form): Implements login form and it's functionality and use hook-form to generate

--- src/components/index.js
+++ src/components/index.js
@@ -4,5 +4,6 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
 
-export { Header, Footer, Container, Logo, LogoutBtn };
+export { Header, Footer, Container, Logo, LogoutBtn, Button };
