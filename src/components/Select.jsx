2452d5eb417cfc18a9f8189dd98a77936373a459
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-24 12:07:58 +0600

    feat(forwardRef): Implements forwardRef at select file

---
+++ src/components/Select.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React, { useId } from "react";
+
+const Select = ({ options, label, className, ...props }, ref) => {
+  const id = useId();
+  return <div></div>;
+};
+
+export default React.forwardRef(Select);
