11b4fb9b9e4776b0185931cbce694bd91b61cf6a
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-25 03:30:08 +0600

    feat(authentication): create protected file to create user authentication

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,33 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+const Protected = ({ children, authentication = true }) => {
+  const navigate = useNavigate();
+  const [loader, setLoader] = useState();
+  const authStatus = useSelector((state) => state.auth.status);
+  useEffect(() => {
+    //TODO: make it more easy to understand
+
+    // if (authStatus ===true){
+    //     navigate("/")
+    // } else if (authStatus === false) {
+    //     navigate("/login")
+    // }
+
+    //let authValue = authStatus === true ? true : false
+
+    if (authentication && authStatus !== authentication) {
+      navigate("/login");
+    } else if (!authentication && authStatus !== authentication) {
+      navigate("/");
+    }
+    setLoader(false);
+  }, [authStatus, navigate, authentication]);
+
+  return loader ? <h1>Loading...</h1> : <>{children}</>;
+};
+
+export default Protected;
