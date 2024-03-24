3435d0e227088cf987a74a987e7c32b946d126aa
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-25 02:03:09 +0600

    feat(useForm): install react-hook-form to implements login form

---
+++ src/components/Login.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm();
+  const [error, setError] = useState();
+
+  return <div>Login</div>;
+};
+
+export default Login;
