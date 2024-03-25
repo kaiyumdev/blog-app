46da1c6e2b2034faefa641600774477ef372d16e
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-26 01:42:28 +0600

    feat(RTE): create RTE for editor

---
+++ src/components/RTE.jsx
@@ -0,0 +1,9 @@
+/* eslint-disable react/prop-types */
+/* eslint-disable no-unused-vars */
+import React from "react";
+
+const RTE = ({ name, control, label, defaultValue = "" }) => {
+  return <div>RTE</div>;
+};
+
+export default RTE;
