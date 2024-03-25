c0e422c57907706f6b8b6c2705c51e942d4c33bf
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-26 01:49:32 +0600

    feat(RTE): export RTE file by default at index.js

--- src/components/index.js
+++ src/components/index.js
@@ -5,5 +5,6 @@ import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button";
+import RTE from "./RTE";
 
-export { Header, Footer, Container, Logo, LogoutBtn, Button };
+export { Header, Footer, Container, Logo, LogoutBtn, Button, RTE };
