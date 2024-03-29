253e665bc8251727f5dac153b98c61e1d0f605f3
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-30 05:48:39 +0600

    feat(login): create login component to implements login button

---
+++ src/pages/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Login as loginComponent } from "../components";
+
+const Login = () => {
+  return (
+    <div className="py-8">
+      <loginComponent></loginComponent>
+    </div>
+  );
+};
+
+export default Login;
