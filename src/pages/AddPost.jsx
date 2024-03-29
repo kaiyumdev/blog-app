a7547ffbaa382482318e41b5bc95c1aab1a679ad
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-30 05:55:42 +0600

    feat(addPost): Implements postform for addPost

---
+++ src/pages/AddPost.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, PostForm } from "../components";
+
+const AddPost = () => {
+  return (
+    <div>
+      <Container>
+        <PostForm></PostForm>
+      </Container>
+    </div>
+  );
+};
+
+export default AddPost;
