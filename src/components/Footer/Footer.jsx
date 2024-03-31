3035f47b225f22f5a463d608cbd28e659847105e
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-31 19:26:24 +0600

    feat(Footer): Implements footer for blog-app

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,75 +1,136 @@
-/* eslint-disable react/prop-types */
 import React from "react";
 import { Link } from "react-router-dom";
 import Logo from "../Logo";
 
-const commonLinkclassName =
-  "text-base font-medium text-white/60 text-xs hover:text-white/60";
-
-const FooterLink = ({ to, text }) => (
-  <li className="mb-4">
-    <Link className={commonLinkclassName} to={to}>
-      {text}
-    </Link>
-  </li>
-);
-
 function Footer() {
-  const navigate = (url) => {
-    window.open(url, "_blank");
-  };
-
   return (
-    <section className="overflow-hidden pt-10 pb-10 bg-[#00040F] text-sm">
-      <div className="mx-auto max-w-7xl px-4">
+    <section className="relative overflow-hidden py-10 bg-gray-400 border border-t-2 border-t-black">
+      <div className="relative z-10 mx-auto max-w-7xl px-4">
         <div className="-m-6 flex flex-wrap">
-          <div className="w-full p-6 md:w-full lg:w-5/12">
+          <div className="w-full p-6 md:w-1/2 lg:w-5/12">
             <div className="flex h-full flex-col justify-between">
               <div className="mb-4 inline-flex items-center">
                 <Logo width="100px" />
               </div>
               <div>
                 <p className="text-sm text-gray-600">
-                  &copy; Copyright 2024. All Rights Reserved.
+                  &copy; Copyright 2023. All Rights Reserved by DevUI.
                 </p>
               </div>
             </div>
           </div>
-          <div className="w-full p-6 md:w-1/3 lg:w-2/12">
+          <div className="w-full p-6 md:w-1/2 lg:w-2/12">
             <div className="h-full">
-              <h3 className="tracking-px mb-9 text-xs font-semibold uppercase text-white">
+              <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-gray-500">
                 Company
               </h3>
               <ul>
-                <FooterLink to="/" text="Features" />
-                <FooterLink to="/" text="Pricing" />
-                <FooterLink to="/" text="Affiliate Program" />
-                <FooterLink to="/" text="Press Kit" />
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    to="/"
+                  >
+                    Features
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    to="/"
+                  >
+                    Pricing
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    to="/"
+                  >
+                    Affiliate Program
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    to="/"
+                  >
+                    Press Kit
+                  </Link>
+                </li>
               </ul>
             </div>
           </div>
-          <div className="w-full p-6 md:w-1/3 lg:w-2/12">
+          <div className="w-full p-6 md:w-1/2 lg:w-2/12">
             <div className="h-full">
-              <h3 className="tracking-px mb-9 text-xs font-semibold uppercase text-white">
+              <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-gray-500">
                 Support
               </h3>
               <ul>
-                <FooterLink to="/" text="Account" />
-                <FooterLink to="/" text="Help" />
-                <FooterLink to="/" text="Contact Us" />
-                <FooterLink to="/" text="Customer Support" />
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    to="/"
+                  >
+                    Account
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    to="/"
+                  >
+                    Help
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    to="/"
+                  >
+                    Contact Us
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    to="/"
+                  >
+                    Customer Support
+                  </Link>
+                </li>
               </ul>
             </div>
           </div>
-          <div className="w-full p-6 md:w-1/3 lg:w-3/12">
+          <div className="w-full p-6 md:w-1/2 lg:w-3/12">
             <div className="h-full">
-              <h3 className="tracking-px mb-9 text-xs font-semibold uppercase text-white">
+              <h3 className="tracking-px mb-9  text-xs font-semibold uppercase text-gray-500">
                 Legals
               </h3>
               <ul>
-                <FooterLink to="/" text="Terms & Conditions" />
-                <FooterLink to="/" text="Privacy Policy" />
-                <FooterLink to="/" text="Licensing" />
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    to="/"
+                  >
+                    Terms &amp; Conditions
+                  </Link>
+                </li>
+                <li className="mb-4">
+                  <Link
+                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    to="/"
+                  >
+                    Privacy Policy
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    className=" text-base font-medium text-gray-900 hover:text-gray-700"
+                    to="/"
+                  >
+                    Licensing
+                  </Link>
+                </li>
               </ul>
             </div>
           </div>
