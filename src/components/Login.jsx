0a9c3d93d3b1efd6444e8c401127eb9bf803089c
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-31 15:37:32 +0600

    feat(login): Create login component for implementing login authentication user

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,21 +1,21 @@
-/* eslint-disable no-unused-vars */
 import React, { useState } from "react";
-import { useForm } from "react-hook-form";
-import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
-import authService from "../appwrite/auth";
 import { login as authLogin } from "../store/authSlice";
-import Logo from "./Logo";
-import Input from "./Input";
-import Button from "./Button";
+import authService from "../appwrite/auth";
+import { Logo, Button, Input } from "./index";
+import { useDispatch } from "react-redux";
+import { useForm } from "react-hook-form";
+import { Loader } from "./index";
 
-const Login = () => {
+function Login() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const { register, handleSubmit } = useForm();
-  const [error, setError] = useState();
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
 
   const login = async (data) => {
+    setLoading(true);
     setError("");
     try {
       const session = await authService.login(data);
@@ -23,17 +23,19 @@ const Login = () => {
         const userData = await authService.getCurrentUser();
         if (userData) {
           dispatch(authLogin(userData));
-          navigate("/");
         }
+        navigate("/all-posts");
       }
     } catch (error) {
       setError(error.message);
+    } finally {
+      setLoading(false);
     }
   };
   return (
-    <div className="flex items-center justify-center w-full">
+    <div className="flex items-center justify-center md:min-h-[80vh]">
       <div
-        className={`mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10`}
+        className={`mx-auto w-[85%] md:w-full md:max-w-[28rem] rounded-xl p-5 md:p-10 bg-customGray shadow-grayBorder`}
       >
         <div className="mb-2 flex justify-center">
           <span className="inline-block w-full max-w-[100px]">
@@ -43,22 +45,24 @@ const Login = () => {
         <h2 className="text-center text-2xl font-bold leading-tight">
           Sign in to your account
         </h2>
-        <p className="mt-2 text-center text-base text-black/60">
+        <p className="mt-2 text-center text-base text-white/60">
           Don&apos;t have any account?&nbsp;
           <Link
             to="/signup"
-            className="font-medium text-primary transition-all duration-200 hover:underline"
+            className="font-medium text-white/80 hover:text-white transition-all duration-200 hover:underline"
           >
             Sign Up
           </Link>
         </p>
         {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+
         <form onSubmit={handleSubmit(login)} className="mt-8">
           <div className="space-y-5">
             <Input
               label="Email: "
               placeholder="Enter your email"
               type="email"
+              className="focus:border-solid focus:border-x-8 focus:border-customPink"
               {...register("email", {
                 required: true,
                 validate: {
@@ -72,18 +76,29 @@ const Login = () => {
               label="Password: "
               type="password"
               placeholder="Enter your password"
+              className="focus:border-solid focus:border-x-8 focus:border-customPink"
               {...register("password", {
                 required: true,
               })}
             />
-            <Button type="submit" className="w-full">
-              Sign in
-            </Button>
+            {loading ? (
+              <div className="w-full grid place-items-center">
+                {" "}
+                <Loader></Loader>
+              </div>
+            ) : (
+              <Button
+                type="submit"
+                className="my-4 py-2 px-5 w-full text-white bg-customPink button-custom rounded-xl shadow-lg hover:bg-[#EFFF3A] hover:text-black duration-400 hover:cursor-pointer"
+              >
+                Sign in
+              </Button>
+            )}
           </div>
         </form>
       </div>
     </div>
   );
-};
+}
 
 export default Login;
