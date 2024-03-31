49ce38636fc25aa939fb9d8004cf2fb934aa0252
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-04-01 00:18:02 +0600

    feat(main.jsx): updated main.jsx to implementing router

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,16 @@ import "./index.css";
 import { Provider } from "react-redux";
 import store from "./store/store.js";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Login from "./pages/Login.jsx";
-import Signup from "./pages/Signup.jsx";
-import EditPost from "./pages/EditPost.jsx";
-import Post from "./pages/Post.jsx";
-import AddPost from "./pages/AddPost.jsx";
 import Home from "./pages/Home.jsx";
-import AllPosts from "./pages/AllPosts.jsx";
-import { AuthLayout } from "./components/index.js";
+import { AuthLayout, Login } from "./components/index.js";
+
+import AddPost from "./pages/AddPost";
+import Signup from "./pages/Signup";
+import EditPost from "./pages/EditPost";
+
+import Post from "./pages/Post";
+
+import AllPosts from "./pages/AllPosts";
 
 const router = createBrowserRouter([
   {
@@ -44,7 +46,7 @@ const router = createBrowserRouter([
         element: (
           <AuthLayout authentication>
             {" "}
-            <AllPosts></AllPosts>
+            <AllPosts />
           </AuthLayout>
         ),
       },
