f7d18c7b7746219aa0cab77828b8677bec2234fe
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-31 15:28:01 +0600

    feat(logout): Create logout button for logout user

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,24 +1,35 @@
-/* eslint-disable no-unused-vars */
-import React from "react";
-import { useDispatch } from "react-redux";
+import React, { useState } from "react";
 import authService from "../../appwrite/auth";
+import { useDispatch } from "react-redux";
 import { logout } from "../../store/authSlice";
+import Loader from "../Loader";
 
-const LogoutBtn = () => {
+function LogoutBtn() {
   const dispatch = useDispatch();
-  const logoutHandler = () => {
-    authService.logout().then(() => {
+  const [loading, setLoading] = useState(false);
+
+  const logoutHandler = async () => {
+    try {
+      setLoading(true);
+      await authService.logout();
       dispatch(logout());
-    });
+    } catch (error) {
+      console.error("Logout failed:", error);
+    } finally {
+      setLoading(false);
+    }
   };
+
   return (
-    <div
-      className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
-      onClick={logoutHandler}
-    >
-      LogoutBtn
-    </div>
+    <>
+      <button
+        className="py-2 px-5 bg-customPink text-white rounded-xl shadow-lg duration-400 hover:drop-shadow-2xl hover:bg-white hover:text-black hover:cursor-pointer"
+        onClick={logoutHandler}
+      >
+        Logout
+      </button>
+    </>
   );
-};
+}
 
 export default LogoutBtn;
