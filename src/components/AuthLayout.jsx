17ae597386b61a3f19fe88325da6ad243068c0ca
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-04-01 00:05:15 +0600

    feat(privateRoute): Create authLayout for implementing privateRoute

--- src/components/AuthLayout.jsx
+++ src/components/AuthLayout.jsx
@@ -1,22 +1,31 @@
 /* eslint-disable react/prop-types */
 import React, { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-function Protected({ children, authentication = true }) {
+export default function Protected({ children, authentication = true }) {
   const navigate = useNavigate();
   const [loader, setLoader] = useState(true);
   const authStatus = useSelector((state) => state.auth.status);
 
   useEffect(() => {
-    if (authentication && authStatus != authentication) {
+    //TODO: make it more easy to understand
+
+    // if (authStatus ===true){
+    //     navigate("/")
+    // } else if (authStatus === false) {
+    //     navigate("/login")
+    // }
+
+    //let authValue = authStatus === true ? true : false
+
+    if (authentication && authStatus !== authentication) {
       navigate("/login");
     } else if (!authentication && authStatus !== authentication) {
       navigate("/");
     }
     setLoader(false);
   }, [authStatus, navigate, authentication]);
-  return loader ? <div>Loading...</div> : <div>{children}</div>;
-}
 
-export default Protected;
+  return loader ? <h1>Loading...</h1> : <>{children}</>;
+}
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,23 +1,20 @@
 /* eslint-disable react/prop-types */
 import React from "react";
 
-function Button({
+export default function Button({
   children,
   type = "button",
-  bgColor = "",
-  textColor = "",
+  bgColor = "bg-blue-600",
+  textColor = "text-white",
   className = "",
   ...props
 }) {
   return (
     <button
-      className={`px-2 py-5 ${bgColor} ${textColor} ${className} `}
-      type={type}
+      className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`}
       {...props}
     >
       {children}
     </button>
   );
 }
-
-export default Button;
