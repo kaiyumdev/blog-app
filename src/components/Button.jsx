b19b9f8e0fa92906787272d13ea4eb3a6a495073
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-31 15:45:27 +0600

    feat(Button): Create Button component for using button dynamically

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,21 +1,23 @@
-/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import React from "react";
 
-export default function Button({
+function Button({
   children,
   type = "button",
-  bgColor = "bg-blue-600",
-  textColor = "text-white",
+  bgColor = "",
+  textColor = "",
   className = "",
   ...props
 }) {
   return (
     <button
-      className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`}
+      className={`px-2 py-5 ${bgColor} ${textColor} ${className} `}
+      type={type}
       {...props}
     >
       {children}
     </button>
   );
 }
+
+export default Button;
