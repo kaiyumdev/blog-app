9d5c753917d7c5e6d27bc90538f8b4871b06bd37
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-31 19:43:38 +0600

    feat(Logo): Create logo for blog-app

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,11 +1,20 @@
+// import React from "react";
+
+// function Logo() {
+//   return (
+//     <div>
+//       <h1 className="font-bold text-pink-500 text-lg">BlogApp</h1>
+//     </div>
+//   );
+// }
+
+// export default Logo;
+
+/* eslint-disable react/prop-types */
 import React from "react";
 
-function Logo() {
-  return (
-    <div>
-      <h1 className="font-bold text-pink-500 text-lg">BlogApp</h1>
-    </div>
-  );
+function Logo({ width = "100px" }) {
+  return <div>Logo</div>;
 }
 
 export default Logo;
