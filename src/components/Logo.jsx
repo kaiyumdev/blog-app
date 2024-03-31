dd61f98539b1bd978d4f0ac1be6d57a84f42ed5d
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-31 15:36:25 +0600

    feat(logo): Create logo for project name

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,9 +1,11 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/prop-types */
 import React from "react";
 
-function Logo({ width = "100px" }) {
-  return <div>Logo</div>;
+function Logo() {
+  return (
+    <div>
+      <h1 className="font-bold text-pink-500 text-lg">BlogApp</h1>
+    </div>
+  );
 }
 
 export default Logo;
