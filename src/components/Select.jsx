d2f42f98e54afd452b8db98e4f125f36ffaf4de4
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-31 19:37:49 +0600

    feat(Select): Create select component for select option for form submition

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,19 +1,14 @@
 /* eslint-disable react/prop-types */
 import React, { useId } from "react";
 
-function Select({ options, label, className = "", ...props }, ref) {
-  const Id = useId();
+function Select({ options, label, className, ...props }, ref) {
+  const id = useId();
   return (
     <div className="w-full">
-      {label && (
-        <label htmlFor={Id} className="">
-          {" "}
-        </label>
-      )}
+      {label && <label htmlFor={id} className=""></label>}
       <select
         {...props}
-        name=""
-        id={Id}
+        id={id}
         ref={ref}
         className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
       >
