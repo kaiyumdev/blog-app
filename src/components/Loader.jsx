62ce2bc7c4d716e7d77bbc9ec4348d5f3bc371b7
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-31 15:39:23 +0600

    feat(Loader): Create loader component for implementing loading by default

---
+++ src/components/Loader.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/no-unknown-property */
+import React from "react";
+
+function Loader() {
+  return (
+    <div class="flex flex-row gap-2">
+      <div class="w-4 h-4 rounded-full bg-customPink animate-bounce"></div>
+      <div class="w-4 h-4 rounded-full bg-customPink animate-bounce [animation-delay:-.1s]"></div>
+      <div class="w-4 h-4 rounded-full bg-customPink animate-bounce [animation-delay:-.2s]"></div>
+    </div>
+  );
+}
+
+export default Loader;
