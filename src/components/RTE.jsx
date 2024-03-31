f1560677f7ce200472a56199646611006bdc2029
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-31 19:40:54 +0600

    feat(RTE): Create RTE to implements real-time-editor for edit-post

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -13,7 +13,7 @@ export default function RTE({ name, control, label, defaultValue = "" }) {
         control={control}
         render={({ field: { onChange } }) => (
           <Editor
-            apiKey="4gl25x788cncajq1ndmn4abgxpdardimn7ycopqgccdsh7gk"
+            apiKey="f5bvowun6dr4zxa7hwlg5zdhxphmfrgig5wdh69yl4vamq89"
             initialValue={defaultValue}
             init={{
               initialValue: defaultValue,
