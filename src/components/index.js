6144c80242082ea102d88f0149ad8a5cf0d6316c
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-31 15:42:07 +0600

    feat(Index): Create Input component for implementing dynamically export file

--- src/components/index.js
+++ src/components/index.js
@@ -1,17 +1,18 @@
-/* eslint-disable no-unused-vars */
+import Select from "./Select";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-import Container from "./container/Container";
+import Container from "./Container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
-import Button from "./Button";
 import RTE from "./RTE";
-import Input from "./Input";
-import Select from "./Select";
 import Signup from "./Signup";
 import Login from "./Login";
-import PostForm from "../components/post-form/PostForm";
+import Button from "./Button/Button";
+import PostForm from "./PostForm/PostForm";
+import PostCard from "./PostCard";
 import AuthLayout from "./AuthLayout";
+import Input from "./Input";
+import Loader from "./Loader";
 
 export {
   Header,
@@ -19,12 +20,14 @@ export {
   Container,
   Logo,
   LogoutBtn,
-  Button,
   RTE,
-  Input,
-  Select,
   Signup,
   Login,
+  Button,
   PostForm,
+  PostCard,
   AuthLayout,
+  Input,
+  Select,
+  Loader,
 };
