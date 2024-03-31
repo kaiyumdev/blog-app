89217cadf3542bf75b8559bac8680b99a2144458
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-31 19:42:19 +0600

    feat(PostCard): Create postCard for implements single post-card

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -2,30 +2,19 @@
 import React from "react";
 import appwriteService from "../appwrite/config";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
-function PostCard({ $id, title, featuredImage, author, likes }) {
+function PostCard({ $id, title, featuredImage }) {
   return (
     <Link to={`/post/${$id}`}>
-      <div className="w-full flex flex-col items-center text-center card-hover rounded-xl p-4 h-full">
-        <div className="w-full flex justify-center mb-4 h-[80%]">
+      <div className="w-full bg-gray-100 rounded-xl p-4">
+        <div className="w-full justify-center mb-4">
           <img
             src={appwriteService.getFilePreview(featuredImage)}
             alt={title}
-            className="rounded-xl h-full object-cover"
+            className="rounded-xl"
           />
         </div>
-        <h2 className="text-lg font-bold">{title}</h2>
-        <div className="flex items-center gap-10">
-          <p className="text-gray-500 justify-left">
-            {author ? author : "Anonymous"}
-          </p>
-          <div className="flex items-center text-sm">
-            <FontAwesomeIcon icon={faHeart} className="mr-1" />
-            <span className="text-sm">{likes}</span>
-          </div>
-        </div>
+        <h2 className="text-xl font-bold">{title}</h2>
       </div>
     </Link>
   );
