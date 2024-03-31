1be368f3656023377a67a1a96b5954953780d16d
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-31 15:32:09 +0600

    feat(RTE): Create RTE component for implements real-time-editing functionality

--- src/components/RTE.jsx
+++ src/components/RTE.jsx
@@ -1,10 +1,9 @@
 /* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
-import { Editor } from "@tinymce/tinymce-react";
 import React from "react";
+import { Editor } from "@tinymce/tinymce-react";
 import { Controller } from "react-hook-form";
 
-const RTE = ({ name, control, label, defaultValue = "" }) => {
+export default function RTE({ name, control, label, defaultValue = "" }) {
   return (
     <div className="w-full">
       {label && <label className="inline-block mb-1 pl-1">{label}</label>}
@@ -14,6 +13,7 @@ const RTE = ({ name, control, label, defaultValue = "" }) => {
         control={control}
         render={({ field: { onChange } }) => (
           <Editor
+            apiKey="4gl25x788cncajq1ndmn4abgxpdardimn7ycopqgccdsh7gk"
             initialValue={defaultValue}
             init={{
               initialValue: defaultValue,
@@ -52,6 +52,4 @@ const RTE = ({ name, control, label, defaultValue = "" }) => {
       />
     </div>
   );
-};
-
-export default RTE;
+}
