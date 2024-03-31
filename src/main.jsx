7ea7373dc3b8d8cf2a20cc50137a8cc16019d09d
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-31 16:00:47 +0600

    feat(main): updated main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,16 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { Provider } from "react-redux";
+import store from "./store/store.js";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Home from "./pages/Home.jsx";
-import { AuthLayout, Login } from "./components/index.js";
-import AllPosts from "./pages/AllPosts.jsx";
-import AddPost from "./pages/AddPost.jsx";
+import Login from "./pages/Login.jsx";
+import Signup from "./pages/Signup.jsx";
+import AllPosts from "./pages/AllPost.jsx";
 import EditPost from "./pages/EditPost.jsx";
 import Post from "./pages/Post.jsx";
-import Signup from "./pages/Signup.jsx";
-import { store } from "./store/store";
+import AddPost from "./pages/AddPost.jsx";
+import Home from "./pages/Home.jsx";
+import { AuthLayout } from "./Components/index.js";
 
 const router = createBrowserRouter([
   {
