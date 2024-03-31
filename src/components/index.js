5a343106b83e7101707b5008c8a9dbe0c8316808
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-31 19:48:26 +0600

    feat(index.js): Create index.js file for default export

--- src/components/index.js
+++ src/components/index.js
@@ -1,19 +1,17 @@
 import Select from "./Select";
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./container/Container";
 import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import RTE from "./RTE";
 import Signup from "./Signup";
 import Login from "./Login";
+import Button from "./Button";
+import PostForm from "./post-form/PostForm";
 import PostCard from "./PostCard";
 import AuthLayout from "./AuthLayout";
 import Input from "./Input";
-import Loader from "./Loader";
-import Button from "./Button";
-import Container from "./container/Container";
-import PostForm from "./post-form/PostForm";
-
 export {
   Header,
   Footer,
@@ -29,5 +27,4 @@ export {
   AuthLayout,
   Input,
   Select,
-  Loader,
 };
