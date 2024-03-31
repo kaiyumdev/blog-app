b3aed4e6ec9887f20f01f8f4572edd3e68d88d19
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-31 19:47:17 +0600

    feat(Input): Create Input component for dynamically use Input component for form submit field

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 import React, { useId } from "react";
+
 const Input = React.forwardRef(function Input(
   { label, type = "text", className = "", ...props },
   ref
@@ -8,17 +9,16 @@ const Input = React.forwardRef(function Input(
   return (
     <div className="w-full">
       {label && (
-        <label className="mb-1 pl-1 inline-block" htmlFor={id}>
-          {" "}
-          {label}{" "}
+        <label className="inline-block mb-1 pl-1" htmlFor={id}>
+          {label}
         </label>
       )}
       <input
         type={type}
-        className={`${className} px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full  `}
-        id={id}
+        className={`px-3 py-2 rounded-lg bg-white text-black outline-none focus:bg-gray-50 duration-200 border border-gray-200 w-full ${className}`}
         ref={ref}
         {...props}
+        id={id}
       />
     </div>
   );
