4728e952a5c60d5b569e9c30d64180482c7d6468
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-31 15:33:43 +0600

    feat(PostCard): Create postCard component for Implementing post-card functionality

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,24 +1,34 @@
 /* eslint-disable react/prop-types */
-/* eslint-disable no-unused-vars */
 import React from "react";
-import { Link } from "react-router-dom";
 import appwriteService from "../appwrite/config";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHeart } from "@fortawesome/free-solid-svg-icons";
 
-const PostCard = ({ $id, title, featuredImage }) => {
+function PostCard({ $id, title, featuredImage, author, likes }) {
   return (
     <Link to={`/post/${$id}`}>
-      <div className="w-full bg-gray-100 rounded-xl p-4">
-        <div className="w-full justify-center mb-4">
+      <div className="w-full flex flex-col items-center text-center card-hover rounded-xl p-4 h-full">
+        <div className="w-full flex justify-center mb-4 h-[80%]">
           <img
             src={appwriteService.getFilePreview(featuredImage)}
             alt={title}
-            className="rounded-xl"
+            className="rounded-xl h-full object-cover"
           />
         </div>
-        <h2 className="text-xl font-bold">{title}</h2>
+        <h2 className="text-lg font-bold">{title}</h2>
+        <div className="flex items-center gap-10">
+          <p className="text-gray-500 justify-left">
+            {author ? author : "Anonymous"}
+          </p>
+          <div className="flex items-center text-sm">
+            <FontAwesomeIcon icon={faHeart} className="mr-1" />
+            <span className="text-sm">{likes}</span>
+          </div>
+        </div>
       </div>
     </Link>
   );
-};
+}
 
 export default PostCard;
