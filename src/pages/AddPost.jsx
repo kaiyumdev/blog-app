99c53bdbd3d8c9462d9265aba965c7fe9f7815a6
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-04-01 00:11:21 +0600

    feat(addPost): Create addPost page for adding post

--- src/pages/AddPost.jsx
+++ src/pages/AddPost.jsx
@@ -4,11 +4,6 @@ import { Container, PostForm } from "../components";
 function AddPost() {
   return (
     <div className="py-8">
-      <div>
-        <h1 className=" text-[2rem] md:text-[2.5rem] text-center font-semibold">
-          Add Post
-        </h1>
-      </div>
       <Container>
         <PostForm />
       </Container>
