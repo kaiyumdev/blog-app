04c5d8e9943037a2bfd8b0a342ab3f73e617f8ee
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-04-01 00:14:42 +0600

    feat(login): Create login page for user to login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-import { Login as LoginComponent } from "../components";
+import { Login as loginComponent } from "../components";
 function Login() {
   return (
     <div className="py-8">
-      <LoginComponent />
+      <loginComponent />
     </div>
   );
 }
