4e70a69a630dad0466b0a2525845be9615b36d58
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-31 15:55:51 +0600

    feat(EditPost): Create EditPost page for editing post

--- src/pages/EditPost.jsx
+++ src/pages/EditPost.jsx
@@ -1,29 +1,34 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { Container, PostForm } from "../Components";
 import appwriteService from "../appwrite/config";
-import { Container, PostForm } from "../components";
 
-const EditPost = () => {
+function EditPost() {
   const [post, setPost] = useState(null);
   const { slug } = useParams();
   const navigate = useNavigate();
-
   useEffect(() => {
-    appwriteService.getPost(slug).then((post) => {
-      if (post) {
-        setPost(post);
-      } else {
-        navigate("/");
-      }
-    });
+    if (slug) {
+      appwriteService.getPost(slug).then((post) => {
+        if (post) {
+          setPost(post);
+        }
+      });
+    } else navigate("/");
   }, [slug, navigate]);
+
   return post ? (
     <div className="py-8">
+      <div>
+        <h1 className=" text-[2rem] md:text-[2.5rem] text-center font-semibold">
+          Edit Post
+        </h1>
+      </div>
       <Container>
-        <PostForm post={post}></PostForm>
+        <PostForm post={post} />
       </Container>
     </div>
   ) : null;
-};
+}
 
 export default EditPost;
