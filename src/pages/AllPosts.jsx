bd11d6a802eaa3382ac47d3a172763bbc053ad98
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-03-31 15:54:48 +0600

    feat(AllPosts): Create AllPosts page for showing all posts

--- src/pages/AllPosts.jsx
+++ src/pages/AllPosts.jsx
@@ -1,30 +1,75 @@
-import React, { useEffect, useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Container, PostCard, Button, Loader } from "../Components";
 import appwriteService from "../appwrite/config";
-import { Container } from "../components";
-import PostCard from "../components/PostCard";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
 
-const AllPosts = () => {
+function AllPosts() {
   const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const authStatus = useSelector((state) => state.auth.status);
+  const navigate = useNavigate();
+
   useEffect(() => {
-    appwriteService.getPosts([]).then((posts) => {
-      if (posts) {
-        setPosts(posts.documents);
-      }
-    });
+    const delayLoading = setTimeout(() => {
+      appwriteService.getPosts([]).then((posts) => {
+        if (posts) {
+          setPosts(posts.documents);
+          setLoading(false);
+        }
+      });
+    }, 2000);
+
+    return () => clearTimeout(delayLoading);
   }, []);
+
+  const handleAddPostClick = () => {
+    navigate("/add-post");
+  };
+
   return (
     <div className="w-full py-8">
+      <div>
+        <h1 className="text-[2rem] md:text-[2.5rem] text-center font-semibold">
+          All Posts
+        </h1>
+      </div>
       <Container>
-        <div className="flex flex-wrap">
-          {posts.map((post) => (
-            <div key={post.$id} className="p-2 w-1/4">
-              <PostCard post={post}></PostCard>
-            </div>
-          ))}
-        </div>
+        {loading ? (
+          <div className="flex items-center justify-center mt-40 mb-40">
+            <Loader />
+          </div>
+        ) : posts.length > 0 ? (
+          <div className="flex flex-wrap flex-row">
+            {posts.map((post) => (
+              <div key={post.$id} className="p-2 w-full sm:w-1/3 xl:w-1/4">
+                <PostCard {...post} />
+              </div>
+            ))}
+          </div>
+        ) : (
+          <div className="text-center">
+            <p>Nothing to Show</p>
+            {authStatus ? (
+              <Button
+                onClick={handleAddPostClick}
+                className="mt-4 bg-customPink text-white rounded-xl px-5 py-2 hover:bg-white hover:text-black hover:border hover:border-solid hover:border-grayBorder hover:cursor-pointer"
+              >
+                Add Post
+              </Button>
+            ) : (
+              <Button
+                to="/signup"
+                className="mt-4 bg-customPink text-white rounded-xl px-5 py-2 hover:bg-white hover:text-black hover:border hover:border-solid hover:border-grayBorder hover:cursor-pointer"
+              >
+                Signup
+              </Button>
+            )}
+          </div>
+        )}
       </Container>
     </div>
   );
-};
+}
 
 export default AllPosts;
