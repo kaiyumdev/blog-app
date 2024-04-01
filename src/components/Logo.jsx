c4ee6a6990d34c69c313cc7dc9d3626cdc80d5c3
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-04-01 17:35:38 +0600

    feat(logo): updated logo at blog-app

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -1,20 +1,8 @@
-// import React from "react";
-
-// function Logo() {
-//   return (
-//     <div>
-//       <h1 className="font-bold text-pink-500 text-lg">BlogApp</h1>
-//     </div>
-//   );
-// }
-
-// export default Logo;
-
 /* eslint-disable react/prop-types */
 import React from "react";
 
 function Logo({ width = "100px" }) {
-  return <div>Logo</div>;
+  return <div>BlogApp</div>;
 }
 
 export default Logo;
