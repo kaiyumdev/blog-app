33df5079209dd662042cec15ee87cdc45a0b6033
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-04-02 15:03:08 +0600

    feat(title): Change meaning full title

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function Logo({ width = "100px" }) {
   return (
     <div className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full">
-      BlogApp
+      BlogProject
     </div>
   );
 }
