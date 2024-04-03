bfbea79e219cc65968cd266aaf0934f2879e8d86
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-04-03 11:09:26 +0600

    feat(postSlice): create postSlice to manage state of posts

---
+++ src/store/postSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  posts: [],
+};
+
+const postSlice = createSlice({
+  name: "posts",
+  initialState,
+  reducers: {
+    setPosts: (state, action) => {
+      state.posts = action.payload;
+    },
+    addPost: (state, action) => {
+      state.posts.push(action.payload.post);
+    },
+  },
+});
+
+export const { setPosts, addPost } = postSlice.actions;
+
+export default postSlice.reducer;
