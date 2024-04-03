2a1e167c28d258e6317fc435baf832065df5939a
MdKaioumislam5 <afrankaiyum34@gmail.com>
2024-04-03 22:43:55 +0600

    feat(config): updated config file

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -78,7 +78,7 @@ export class Service {
     }
   }
 
-  async getPosts(queries = [Query.equal("status", "active")]) {
+  async getPosts(queries = Query.equal("status", "active")) {
     try {
       return await this.databases.listDocuments(
         conf.appwriteDatabaseId,
