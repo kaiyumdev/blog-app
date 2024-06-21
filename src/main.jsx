cb48f2758b386b5deac46f24dc0c7685134a1ec6
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-21 19:39:20 +0600

    feat(main.jsx): updated main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,90 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import App from "./App.jsx";
+// import "./index.css";
+// import { Provider } from "react-redux";
+// import store from "./store/store.js";
+// import { RouterProvider, createBrowserRouter } from "react-router-dom";
+// import Home from "./pages/Home.jsx";
+// import { AuthLayout, Login } from "./components/index.js";
+
+// import AddPost from "./pages/AddPost";
+// import Signup from "./pages/Signup";
+// import EditPost from "./pages/EditPost";
+
+// import Post from "./pages/Post";
+
+// import AllPosts from "./pages/AllPosts";
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <Home />,
+//       },
+//       {
+//         path: "/login",
+//         element: (
+//           <AuthLayout authentication={false}>
+//             <Login />
+//           </AuthLayout>
+//         ),
+//       },
+//       {
+//         path: "/signup",
+//         element: (
+//           <AuthLayout authentication={false}>
+//             <Signup />
+//           </AuthLayout>
+//         ),
+//       },
+//       {
+//         path: "/all-posts",
+//         element: (
+//           <AuthLayout authentication>
+//             {" "}
+//             <AllPosts />
+//           </AuthLayout>
+//         ),
+//       },
+//       {
+//         path: "/add-post",
+//         element: (
+//           <AuthLayout authentication>
+//             {" "}
+//             <AddPost />
+//           </AuthLayout>
+//         ),
+//       },
+//       {
+//         path: "/edit-post/:slug",
+//         element: (
+//           <AuthLayout authentication>
+//             {" "}
+//             <EditPost />
+//           </AuthLayout>
+//         ),
+//       },
+//       {
+//         path: "/post/:slug",
+//         element: <Post />,
+//       },
+//     ],
+//   },
+// ]);
+
+// ReactDOM.createRoot(document.getElementById("root")).render(
+//   <React.StrictMode>
+//     <Provider store={store}>
+//       <RouterProvider router={router} />
+//     </Provider>
+//   </React.StrictMode>
+// );
+
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
@@ -7,14 +94,7 @@ import store from "./store/store.js";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import { AuthLayout, Login } from "./components/index.js";
-
-import AddPost from "./pages/AddPost";
-import Signup from "./pages/Signup";
-import EditPost from "./pages/EditPost";
-
-import Post from "./pages/Post";
-
-import AllPosts from "./pages/AllPosts";
+import { Signup, AllPosts, AddPost, EditPost, Post } from "./pages/index.js";
 
 const router = createBrowserRouter([
   {
@@ -77,9 +157,9 @@ const router = createBrowserRouter([
 ]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <RouterProvider router={router} />
-    </Provider>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <Provider store={store}>
+    <RouterProvider router={router} />
+  </Provider>
+  // </React.StrictMode>
 );
