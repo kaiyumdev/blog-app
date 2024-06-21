7c4f040f16c79b0dbc679665f8a2f0dcaccd8d8c
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-21 20:05:36 +0600

    feat(logOut): Implements logOut button to click to logOut user

--- src/components/Header/LogoutBtn.jsx
+++ src/components/Header/LogoutBtn.jsx
@@ -1,10 +1,37 @@
+// import React from "react";
+// import { useDispatch } from "react-redux";
+// import authService from "../../appwrite/auth";
+// import { logout } from "../../store/authSlice";
+// import { useNavigate } from "react-router-dom";
+
+// function LogoutBtn() {
+//   const dispatch = useDispatch();
+//   const navigate = useNavigate();
+//   const logoutHandler = () => {
+//     authService.logout().then(() => {
+//       dispatch(logout());
+//       navigate("/");
+//     });
+//   };
+//   return (
+//     <button
+//       className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+//       onClick={logoutHandler}
+//     >
+//       Logout
+//     </button>
+//   );
+// }
+
+// export default LogoutBtn;
+
 import React from "react";
 import { useDispatch } from "react-redux";
 import authService from "../../appwrite/auth";
 import { logout } from "../../store/authSlice";
 import { useNavigate } from "react-router-dom";
 
-function LogoutBtn() {
+function LogoutBtn({ className = "" }) {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const logoutHandler = () => {
@@ -13,9 +40,10 @@ function LogoutBtn() {
       navigate("/");
     });
   };
+
   return (
     <button
-      className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+      className={`inline-block font-semibold mx-6 duration-200 hover:text-textHover rounded-full ${className}`}
       onClick={logoutHandler}
     >
       Logout
