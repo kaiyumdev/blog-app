f6dcd0af9ae7986e197f864067e0238882eed23a
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-21 19:48:11 +0600

    feat(postSlice): updated postSlice to manage states

--- src/store/postSlice.js
+++ src/store/postSlice.js
@@ -1,18 +1,63 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit";
+
+// const initialState = {
+//   posts: [],
+// };
+
+// const postSlice = createSlice({
+//   name: "posts",
+//   initialState,
+//   reducers: {
+//     allPosts: (state, action) => {
+//       state.posts = action.payload;
+//     },
+//     addPost: (state, action) => {
+//       state.posts.push(action.payload.post);
+//     },
+//     removePost: (state, action) => {
+//       state.posts = state.posts.filter((post) => post.id !== action.payload);
+//     },
+//     updatePost: (state, action) => {
+//       const updatedPostIndex = state.posts.findIndex(
+//         (post) => post.$id === action.payload
+//       );
+//       if (updatedPostIndex !== -1) {
+//         state.posts[updatedPostIndex] = action.payload;
+//       }
+//     },
+//   },
+// });
+
+// export const { setPosts, addPost } = postSlice.actions;
+
+// export default postSlice.reducer;
+
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import appwriteService from "../appwrite/config";
 
 const initialState = {
   posts: [],
+  error: null,
 };
 
+export const fetchPosts = createAsyncThunk("fetchPosts", async () => {
+  try {
+    const response = await appwriteService.getPosts();
+    return response.documents;
+  } catch (error) {
+    console.log(error);
+  }
+});
+
 const postSlice = createSlice({
-  name: "posts",
+  name: "post",
   initialState,
   reducers: {
     allPosts: (state, action) => {
       state.posts = action.payload;
     },
     addPost: (state, action) => {
-      state.posts.push(action.payload.post);
+      state.posts.push(action.payload);
     },
     removePost: (state, action) => {
       state.posts = state.posts.filter((post) => post.id !== action.payload);
@@ -26,8 +71,19 @@ const postSlice = createSlice({
       }
     },
   },
+  extraReducers: (builder) => {
+    builder.addCase(fetchPosts.pending, (state) => {
+      state.error = null;
+    });
+    builder.addCase(fetchPosts.fulfilled, (state, action) => {
+      state.posts = action.payload;
+      // update state with fetched data if needed
+    });
+    builder.addCase(fetchPosts.rejected, (state, action) => {
+      state.error = action.error.message;
+    });
+  },
 });
 
-export const { setPosts, addPost } = postSlice.actions;
-
+export const { allPosts, addPost, removePost, updatePost } = postSlice.actions;
 export default postSlice.reducer;
