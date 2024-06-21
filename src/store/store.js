9033c835631259646a1bce2d04d5c1b96eaac157
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-21 19:46:46 +0600

    feat(store): updated store to manage states

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,22 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import authSlice from "./authSlice";
+
+// const store = configureStore({
+//   reducer: {
+//     auth: authSlice,
+//   },
+// });
+
+// export default store;
+
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "./authSlice";
+import postSlice from "./postSlice";
 
 const store = configureStore({
   reducer: {
     auth: authSlice,
+    post: postSlice,
   },
 });
 
