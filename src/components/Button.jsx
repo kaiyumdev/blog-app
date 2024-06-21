5f60daebe8ddd0c0201307bd5ee4ae2713050e4d
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-21 20:02:19 +0600

    feat(updated): updated authLayout, button & index

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,21 +1,46 @@
-/* eslint-disable react/prop-types */
+// /* eslint-disable react/prop-types */
+// import React from "react";
+
+// //dynamic button
+// export default function Button({
+//   children,
+//   type = "button",
+//   bgColor = "bg-blue-600",
+//   textColor = "text-white",
+//   className = "",
+//   ...props
+// }) {
+//   return (
+//     <button
+//       className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`}
+//       {...props}
+//     >
+//       {children}
+//     </button>
+//   );
+// }
+
 import React from "react";
 
-//dynamic button
-export default function Button({
+function Button({
   children,
   type = "button",
   bgColor = "bg-blue-600",
   textColor = "text-white",
   className = "",
+  onclick,
   ...props
 }) {
   return (
     <button
-      className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`}
+      className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className} hover:bg-blue-700 transition duration-200 font-medium`}
       {...props}
+      // onClick={onclick}
     >
       {children}
     </button>
   );
 }
+
+export default Button;
+
