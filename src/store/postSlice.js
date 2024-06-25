11101f2a653b5e30615327e4d41098e323bb24ec
kaiyumdev <afrankaiyum34@gmail.com>
2024-06-25 21:34:57 +0600

    feat(postSlice): remove postSlice for this project

--- src/store/postSlice.js
+++ src/store/postSlice.js
@@ -1,33 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
+// import { createSlice } from "@reduxjs/toolkit";
 
-const initialState = {
-  posts: [],
-};
+// const initialState = {
+//   posts: [],
+// };
 
-const postSlice = createSlice({
-  name: "posts",
-  initialState,
-  reducers: {
-    allPosts: (state, action) => {
-      state.posts = action.payload;
-    },
-    addPost: (state, action) => {
-      state.posts.push(action.payload.post);
-    },
-    removePost: (state, action) => {
-      state.posts = state.posts.filter((post) => post.id !== action.payload);
-    },
-    updatePost: (state, action) => {
-      const updatedPostIndex = state.posts.findIndex(
-        (post) => post.$id === action.payload
-      );
-      if (updatedPostIndex !== -1) {
-        state.posts[updatedPostIndex] = action.payload;
-      }
-    },
-  },
-});
+// const postSlice = createSlice({
+//   name: "posts",
+//   initialState,
+//   reducers: {
+//     allPosts: (state, action) => {
+//       state.posts = action.payload;
+//     },
+//     addPost: (state, action) => {
+//       state.posts.push(action.payload.post);
+//     },
+//     removePost: (state, action) => {
+//       state.posts = state.posts.filter((post) => post.id !== action.payload);
+//     },
+//     updatePost: (state, action) => {
+//       const updatedPostIndex = state.posts.findIndex(
+//         (post) => post.$id === action.payload
+//       );
+//       if (updatedPostIndex !== -1) {
+//         state.posts[updatedPostIndex] = action.payload;
+//       }
+//     },
+//   },
+// });
 
-export const { setPosts, addPost } = postSlice.actions;
+// export const { setPosts, addPost } = postSlice.actions;
 
-export default postSlice.reducer;
+// export default postSlice.reducer;
